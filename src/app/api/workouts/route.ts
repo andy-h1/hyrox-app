5311c7e8aa874a71a8b349d0bcb5a1d24d4693bb
andy-h1 <andy.huynh@kry.se>
2025-09-11 20:58:06 +0100

    feat: adding api/db function to create workout

---
+++ src/app/api/workouts/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createWorkout } from "@/lib/database/workouts";
+
+export async function POST(req: NextRequest) {
+    const data = await req.json();
+    const newWorkout = await createWorkout(data, 1); // Hard-coded userId for now
+
+    return NextResponse.json({ data: newWorkout }, { status: 201 });
+}
---
+++ src/lib/database/workouts.ts
@@ -0,0 +1,53 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const mockWorkoutData = {
+    name: '1k run',
+    type: 'FOR_TRAINING',
+    notes: 'quick session',
+    exercises: [
+        {
+            exerciseId: 1,
+            value: 1000,
+            timeTaken: 270,
+            orderInWorkout: 1,
+        },
+        {
+            exerciseId: 8,
+            value: 50,
+            timeTaken: 135,
+            orderInWorkout: 2,
+        }
+    ]
+}
+
+export async function createWorkout(workoutData: any, userId: number) {
+    const { name, type, notes, exercises } = workoutData;
+
+    // Step 1: Create the workout
+    const newWorkout = await prisma.workout.create({
+
+        data: {
+            userId,
+            date: new Date(),
+            type,
+            notes,
+        }
+    });
+
+    // Step 2: Create workout exercises
+    for (const exercise of exercises) {
+        await prisma.workoutExercise.create({
+            data: {
+                workoutId: newWorkout.id,
+                exerciseId: exercise.exerciseId,
+                value: exercise.value,
+                timeTaken: exercise.timeTaken,
+                orderInWorkout: exercise.orderInWorkout
+            }
+        });
+    }
+
+    return newWorkout;
+}
