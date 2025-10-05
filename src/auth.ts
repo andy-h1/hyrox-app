a8356a4a1398025fb512f59f20343ff56e183d3b
andy-h1 <andy.huynh@kry.se>
2025-10-05 21:18:30 +0100

    chore: remove unused code

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,4 @@
 import NextAuth from 'next-auth';
-import Resend from 'next-auth/providers/resend';
 import Google from 'next-auth/providers/google';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { prisma } from '@/lib/prisma';
