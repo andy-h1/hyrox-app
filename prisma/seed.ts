530bb8a5b749526fd9a46145f1151051debfb6a6
andy-h1 <a.huynh2612@gmail.com>
2025-10-14 23:43:08 +0100

    feat: update seed schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,16 +10,22 @@ async function main() {
     { name: 'Ski Erg', category: 'cardio' },
     { name: 'Row', category: 'cardio' },
     { name: 'Bike', category: 'cardio' },
+    { name: 'Assault Bike', category: 'cardio' },
 
     // Strength
     { name: 'Sled Push', category: 'strength' },
     { name: 'Sled Pull', category: 'strength' },
     { name: 'Burpee Broad Jump', category: 'strength' },
     { name: 'Wall Balls', category: 'strength' },
-    { name: 'Lunges', category: 'strength' },
+    { name: 'Walking Lunges', category: 'strength' },
 
     // Additional Training Exercises
-    { name: 'Squats', category: 'strength' },
+    { name: 'Back Squats', category: 'strength' },
+    { name: 'Front Squats', category: 'strength' },
+    { name: 'Bulgarian Split Squats', category: 'strength' },
+    { name: 'Deadlifts', category: 'strength' },
+    { name: 'Romanian Deadlifts', category: 'strength' },
+
     { name: 'Overhead Press', category: 'strength' },
     { name: 'Pull-ups', category: 'strength' },
     { name: 'Push-ups', category: 'strength' },
@@ -35,6 +41,24 @@ async function main() {
   }
 
   console.log('Seed data created successfully');
+
+  const workoutTemplates = [
+    //
+    {
+      name: 'Hyrox template 1',
+      description: 'Complete as many rounds as you can in 30 mins',
+      createdBy: 1,
+      format: 'AMRAP',
+      duration: 1800,
+
+      exercises: {
+        exerciseId: 1,
+        targetValue: 1000,
+        targetUnit: 'meters',
+        orderIndex: 1,
+      },
+    },
+  ];
 }
 
 main()
